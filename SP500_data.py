5e9e4fdde25e9f5782db73eb89cde3cf6f363eae
chirschmann3 <chirschmann3@gmail.com>
2022-07-27 01:51:01 -0500

    Added data pull from datapackage.

--- SP500_data.py
+++ SP500_data.py
@@ -1,5 +1,15 @@
 import datapackage
-import pandas
+import pandas as pd
 
 
+# code from https://datahub.io/core/s-and-p-500#pandas
+data_url = 'https://datahub.io/core/s-and-p-500/datapackage.json'
 
+# to load Data Package into storage
+package = datapackage.Package(data_url)
+
+# to load only tabular data
+resources = package.resources
+for resource in resources:
+    if resource.tabular:
+        data = pd.read_csv(resource.descriptor['path'])
