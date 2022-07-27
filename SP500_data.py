6f02fd64c7753cf1fd64adb00ab73b53b6eb2751
chirschmann3 <chirschmann3@gmail.com>
2022-07-27 02:49:53 -0500

    Edits to ensure dates are datetimes.

--- SP500_data.py
+++ SP500_data.py
@@ -1,15 +1,26 @@
 import datapackage
 import pandas as pd
 
+class SPYdata():
 
-# code from https://datahub.io/core/s-and-p-500#pandas
-data_url = 'https://datahub.io/core/s-and-p-500/datapackage.json'
+    def __init__(self):
 
-# to load Data Package into storage
-package = datapackage.Package(data_url)
+        return
 
-# to load only tabular data
-resources = package.resources
-for resource in resources:
-    if resource.tabular:
-        data = pd.read_csv(resource.descriptor['path'])
+    def get_data(self):
+        # code from https://datahub.io/core/s-and-p-500#pandas
+        data_url = 'https://datahub.io/core/s-and-p-500/datapackage.json'
+
+        # to load Data Package into storage
+        package = datapackage.Package(data_url)
+
+        # to load only tabular data
+        resources = package.resources
+        for resource in resources:
+            if resource.tabular:
+                data = pd.read_csv(resource.descriptor['path'])
+
+        # change Date column to datetime
+        data['Date'] = pd.to_datetime(data['Date'])
+
+        return data
